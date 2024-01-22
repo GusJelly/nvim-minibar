721548f7bd66e0d5ec41752945683fd4dc0e46dc
GusJelly <gugatpm@gmail.com>
2024-01-22 18:21:05 +0000

    feat(minibar): Added code
    Added code for making the plugin work

--- lua/minibar/init.lua
+++ lua/minibar/init.lua
@@ -7,26 +7,26 @@ M = {}
 -- Gets the current mode when ran
 ---@return string
 local function getCurrentMode()
-  local mode = vim.api.nvim_eval("mode()")
-  local modeName = ""
+    local mode = vim.api.nvim_eval("mode()")
+    local modeName = ""
 
-  if mode == 'n' then
-    modeName = "Normal"
-  elseif mode == 'i' then
-    modeName = "Insert"
-  elseif mode == 'v' then
-    modeName = "Visual"
-  elseif mode == 'V' then
-    modeName = "Visual Line"
-  elseif mode == 'c' then
-    modeName = "Command"
-  elseif mode == 't' then
-    modeName = "Terminal"
-  else
-    modeName = vim.api.nvim_eval('mode()')
-  end
+    if mode == 'n' then
+        modeName = "Normal"
+    elseif mode == 'i' then
+        modeName = "Insert"
+    elseif mode == 'v' then
+        modeName = "Visual"
+    elseif mode == 'V' then
+        modeName = "Visual Line"
+    elseif mode == 'c' then
+        modeName = "Command"
+    elseif mode == 't' then
+        modeName = "Terminal"
+    else
+        modeName = vim.api.nvim_eval('mode()')
+    end
 
-  return modeName
+    return modeName
 end
 
 -- Creates the statusline string
@@ -34,32 +34,36 @@ end
 ---@param branch string
 ---@return string
 local function createStatuslineString(modeName, branch)
-  local statusline = "[" .. modeName .. "] " .. branch .. " %= [%f]%m %= %h%r [%l,%c] [%P %L]"
+    local statusline = "[" .. modeName .. "] " .. branch .. " %= [%f]%m %= %h%r [%l,%c] [%P %L]"
 
-  return statusline
+    return statusline
 end
 
 -- Redraws the statusline
 local function changeStatusline()
-  local branch = ""
+    local branch = ""
 
-  -- Set statusline every time the mode changes
-  vim.api.nvim_create_autocmd("ModeChanged", {
-    callback = function()
-      local modeName = getCurrentMode()
+    -- Set statusline every time the mode changes
+    vim.api.nvim_create_autocmd("ModeChanged", {
+        callback = function()
+            local modeName = getCurrentMode()
 
-      vim.opt_local.statusline = createStatuslineString(modeName, branch)
-    end
-  })
+            vim.opt_local.statusline = createStatuslineString(modeName, branch)
+        end
+    })
 
-  -- Change statusline every time we open a buffer
-  vim.api.nvim_create_autocmd("BufEnter", {
-    callback = function()
-      local modeName = getCurrentMode()
+    -- Change statusline every time we open a buffer
+    vim.api.nvim_create_autocmd("BufEnter", {
+        callback = function()
+            local modeName = getCurrentMode()
 
-      vim.opt_local.statusline = createStatuslineString(modeName, branch)
-    end
-  })
+            vim.opt_local.statusline = createStatuslineString(modeName, branch)
+        end
+    })
+end
+
+function M.setup()
+    changeStatusline()
 end
 
 return M
--- plugin/minibar.lua
+++ plugin/minibar.lua
@@ -1,3 +1,3 @@
-if vim.fn.has("nvim-0.9.0") ~= 1 then
-   vim.api.nvim_err_writeln("Minibar requires at least nvim-0.9.0.")
+if vim.fn.has("lualine") == 1 then
+    vim.api.nvim_err_writeln("Minibar is not to be used with other statusbar plugins")
 end
